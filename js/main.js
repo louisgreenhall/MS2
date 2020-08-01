2787c8941ce335cb0fcad9fe7df9b8b27e049d69
louisgreenhall <lgreenhall.lg@gmail.com>
2020-08-01 21:54:13 +0100

    API Progress - team standings menu now added
    Teams ladder now provides all match fixtures, past and future from click event

--- js/main.js
+++ js/main.js
@@ -1,25 +1,74 @@
+// let apiKeyV2 = "rqz7xyyqush2cn5m838nffdy";
 
-// https://api.sportradar.com/australianrules/trial/v2/en/competitors/sr:competitor:4456/profile.xml?api_key={your_api_key}
+$(() => {
 
+    let standingsUrl = "https://corsaway.herokuapp.com/proxy?url=https://api.sportradar.com/australianrules/trial/v2/en/seasons/sr:season:72434/standings.json?api_key=rqz7xyyqush2cn5m838nffdy";
+    fetch(standingsUrl)
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            for (let team of json.standings[0].groups[0].team_standings) {
 
-function getData() {
+                $(".standings").append(`<tr>
+                                     <td>${team.competitor_standing.rank}</td>
+                                     <td>
+                                         <a href="#" class="test" competitorId="${team.competitor_standing.competitor.id}">${team.competitor_standing.competitor.name}</a>
+                                     </td>
+                                     <td>${team.competitor_standing.win}</td>
+                                     <td>${team.competitor_standing.loss}</td>
+                                     <td>${team.competitor_standing.draw}</td>
+                                 </tr>`);
+            };
+        });
 
-    let aflKey = "rqz7xyyqush2cn5m838nffdy";
-    let aflKey2 = "pzvdp4whtad4ygm5ywv7zhzf"
-    let competitor_id ="sr:competitor:4456";
-    // let aflURL = `http://api.sportradar.com/australianrules/trial/v1/en/competitors/${competitor_id}/profile.json?api_key=${aflKey2}` 
-    var url = "https://api.sportradar.com/football-t1/australian/en/competitors/sr:competitor:4456/profile.json?api_key=pzvdp4whtad4ygm5ywv7zhzf"
-    // RETURN the promise
-    return fetch(url, {
-       headers: {"Access-Control-Allow-Origin": "*"},
-       mode: 'cors'
-    }).then(function(response) {
-        return response.json(); // process it inside the `then`
-    });
-	
-}
+    $("body").on("click", ".test", function (event) {
 
-getData().then(function(response) {
-    // access the value inside the `then`
-    console.log(response)
+        let id = $(event.target).attr("competitorId");
+        fetchSeasonDetails("sr:season:72434").then(function (json) {
+
+            let matches = filterMatchesByTeam(json, id);
+
+            $(".matches").html(""); //clearing in case team fixtures already loaded.
+
+            for (let i = 0; i < matches.length; i++) {
+
+                let match = matches[i];
+                let matchId = match.sport_event.id;
+
+                $(".matches").append(`<li style="padding-left:10px;"> <a href="#" class="create" matchId="${matchId}">${match.sport_event.sport_event_context.stage.round} - ${match.sport_event.competitors[0].abbreviation} vs ${match.sport_event.competitors[1].abbreviation}</a></li>`)
+            }
+
+        });
+
+    })
+
+    function filterMatchesByTeam(json, id) {
+        let matches = [];
+        for (let i = 0; i < json.summaries.length; i++) {
+
+            let match = json.summaries[i];
+
+            if (match.sport_event_status.status === "cancelled") {
+                continue;
+            }
+            if (match.sport_event.competitors[0].id === id || match.sport_event.competitors[1].id === id) {
+                matches.push(match);
+            }
+        }
+
+        return matches;
+    }
+
+    // reusable function to get the season details from the api
+
+    function fetchSeasonDetails(id) {
+
+        return fetch(`https://corsaway.herokuapp.com/proxy?url=https://api.sportradar.com/australianrules/trial/v2/en/seasons/${id}/summaries.json?api_key=rqz7xyyqush2cn5m838nffdy`)
+            .then(function (response) {
+                return response.json();
+            });
+
+    }
+
 });
